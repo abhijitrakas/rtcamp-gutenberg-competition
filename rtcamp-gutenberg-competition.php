99a10e133328c412db14818d1599b1f7a9346554
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2019-02-03 13:16:49 +0530

    Add pluign file

---
+++ rtcamp-gutenberg-competition.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: rtCamp Gutenberg Competition
+Description: Some more Gutenberg blocks.
+Author: Abhijit Rakas
+Version: 1.0.0
+Author URI: https://github.com/abhijitrakas
+*/
